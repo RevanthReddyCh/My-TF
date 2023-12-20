75f33d0e34dd5a5cb09057494615b32b6f62462d
RevanthReddyCh <crr.190295@gmail.com>
2023-12-20 16:44:55 +0530

    final commit for vpc, subnets, route tables and internet gateway

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.My1stVPC.id
   cidr_block              = "10.0.0.0/28"  # Replace with your desired CIDR block for the subnet
   availability_zone       = "ap-south-2a"    # Replace with your desired availability zone
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public_subnet"
@@ -24,6 +25,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.My1stVPC.id
   cidr_block              = "10.0.1.0/28"  # Replace with your desired CIDR block for the subnet
   availability_zone       = "ap-south-2a"    # Replace with your desired availability zone
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "private_subnet"
---
+++ 00-vpc/route_tables.tf
@@ -0,0 +1,32 @@
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.My1stVPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.first_igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.My1stVPC.id
+
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
