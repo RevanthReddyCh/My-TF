7865940b0ff3f195419303ef9057cbd46d5ba9ab
RevanthReddyCh <64233707+RevanthReddyCh@users.noreply.github.com>
2024-07-12 14:36:46 +0530

    Delete Instance-Launch-File directory

--- Instance-Launch-File/gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "usw2_gateway" {
-  vpc_id = aws_vpc.myvpc2.id
-
-  tags = {
-    Name = "usw2-igw"
-  }
-}
--- Instance-Launch-File/inventory.tpl
+++
@@ -1,2 +0,0 @@
-node01 ansible_host=${node01} ansible_user=ubuntu
-node02 ansible_host=${node02} ansible_user=ubuntu
